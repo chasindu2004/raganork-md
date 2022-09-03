76ce45c136ef4463a52d171a906d24b06906de12
chasindu2004 <110662608+chasindu2004@users.noreply.github.com>
2022-09-03 20:35:36 +0530

    Update SQ-QA.js

--- SQ-QA.js
+++ SQ-QA.js
@@ -3,5 +3,3 @@ module.exports = {
    SEW_QUEEN_TEAM: '94714366177,94766911891,94711031915,94757222271,94713179802', // Team - SEW QUEEN TEAM 
    QUEEN_AMDI_OWNER: '94757405652' // Black Amda - Queen Amdi Owner --> For The Friendship 
     } 
-Footer
-© 2022 GitHub, Inc.
